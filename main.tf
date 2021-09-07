21d868cb4c904c32f87c56d2f1a682d2f167597c
Justin Casso <justincasso1@gmail.com>
2021-09-06 17:46:20 -0700

    Remove unnecessary backend block from module

--- main.tf
+++ main.tf
@@ -5,12 +5,6 @@ terraform {
       version = "~> 3.0"
     }
   }
-
-  backend "s3" {
-    bucket = "terraform-applications"
-    key    = "mean-demo/terraform.tfstate"
-    region = "us-east-1"
-  }
 }
 
 provider "aws" {
