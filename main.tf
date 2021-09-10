9c66654b777811724f5f9e586a2fab6e6c245712
Justin Casso <justincasso1@gmail.com>
2021-09-09 23:12:32 -0700

    Remove operations connascence of name from module

--- main.tf
+++ main.tf
@@ -21,14 +21,8 @@ data "terraform_remote_state" "operations" {
   }
 }
 
-locals {
-  vpc_id = var.operation == "stage" ? data.terraform_remote_state.operations.outputs.stage_vpc_id : data.terraform_remote_state.operations.outputs.prod_vpc_ids
-  sg_ids = var.operation == "stage" ? data.terraform_remote_state.operations.outputs.stage_sg_ids : data.terraform_remote_state.operations.outputs.prod_sg_ids
-  hosted_zone = var.operation == "stage" ? data.terraform_remote_state.operations.outputs.stage_hosted_zone : data.terraform_remote_state.operations.outputs.prod_hosted_zone
-}
-
 resource "aws_route53_zone" "app_zone" {
-  name = "${var.name}.${local.hosted_zone.name}"
+  name = "${var.name}.${data.terraform_remote_state.operations.outputs.hosted_zone.name}"
 
   tags = {
     ops = var.name
@@ -37,10 +31,10 @@ resource "aws_route53_zone" "app_zone" {
 
 resource "aws_route53_record" "operations_ns_for_app" {
   allow_overwrite = true
-  name            = "${var.name}.${local.hosted_zone.name}"
+  name            = "${var.name}.${data.terraform_remote_state.operations.outputs.hosted_zone.name}"
   ttl             = 60
   type            = "NS"
-  zone_id         = local.hosted_zone.id
+  zone_id         = data.terraform_remote_state.operations.outputs.hosted_zone.id
 
   records = [
     aws_route53_zone.app_zone.name_servers[0],
